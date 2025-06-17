29742ef3c7a603b7a1c01c673a99da870db49211
JordannM83 <11457@holbertonstudents.com>
2025-06-17 13:46:51 +0200

    feat: add program to print lowercase and uppercase alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
  * main - Entry point
  * Description: This program prints the lowercase alphabet
- * followed by a new line.
+ * and in uppercase followed by a new line.
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
+	}
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	{
+	putchar(letter);
 	}
 	putchar('\n');
 	return (0);
