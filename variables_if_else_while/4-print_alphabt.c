b8fe8e9b02fff13b08f8e02a021b93132f697719
JordannM83 <11457@holbertonstudents.com>
2025-06-17 13:57:31 +0200

    feat: add program to print lowercase alphabet without q and e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: This program prints the lowercase alphabet
+ * without q and e, followed by a new line.
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+	if (letter != 'q' && letter != 'e')
+	{
+		putchar(letter);
+	}
+}
+putchar('\n');
+return (0);
+}
