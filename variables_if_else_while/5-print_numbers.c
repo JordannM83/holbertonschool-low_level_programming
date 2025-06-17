736f0b6e7244904178c2f2e124ce60fa5ad7c1ac
JordannM83 <11457@holbertonstudents.com>
2025-06-17 14:23:19 +0200

    feat: add programs to print single digit numbers and include necessary headers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: print all single digit numbers
+ * of base 10 starting form 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+	putchar('\n');
+	return (0);
+}
+}
