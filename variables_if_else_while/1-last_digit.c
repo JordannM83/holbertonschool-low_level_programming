4f8ab3767127e779198c32dd8926b70636f0c9de
JordannM83 <11457@holbertonstudents.com>
2025-06-17 09:53:18 +0200

    feat: add initial implementation of last_digit program

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: This program generates a random number,
+ * checks the last digit, and prints whether it is greater than 5,
+ * less than 6 and not 0, or 0.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if
+	return (0);
+}
