8ca1afd0cfbfc8922931e37dcf15b161b4affb1e
JordannM83 <11457@holbertonstudents.com>
2025-06-17 11:24:59 +0200

    fix: correct last digit calculation and update output messages in last_digit program

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,16 +14,17 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is ", n);
-if (n > 5)
+int last_digit = n % 10;
+printf("Last digit of %d is ", last_digit);
+if (last_digit > 5)
 {
 printf("and is greater than 5");
 }
-else if (n == 0)
+else if (last_digit == 0)
 {
 printf("and is 0");
 }
-else if (n < 6 && n != 0)
+else if (last_digit < 6 && n != 0)
 {
 	printf("and is less than 6 and not 0");
 }
