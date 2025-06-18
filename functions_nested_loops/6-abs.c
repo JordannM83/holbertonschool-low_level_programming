ea84b2c79d22f10d9226a2fe7609d4cdbe330375
JordannM83 <11457@holbertonstudents.com>
2025-06-18 15:10:02 +0200

    feat: implement _abs function to compute absolute value and add main for testing

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer whose absolute value is to be computed.
+ * Return: The absolute value of the integer.
+ */
+
+
+int _abs(int n)
+{
+	return (abs(n));
+}
