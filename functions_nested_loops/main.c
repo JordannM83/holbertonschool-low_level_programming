8f3de9aa7da691e1183598447715c15bf7186569
JordannM83 <11457@holbertonstudents.com>
2025-06-18 10:58:30 +0200

    refactor: clean up comments and formatting in main function

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-/**
- * main - check the code
- * Description: This program prints the lowercase alphabet
- * followed by a new line.
- * Return: Always 0.
- */
+
+// Déclaration de la fonction print_alphabet
 void print_alphabet(void);
 
 int main(void)
 {
-	print_alphabet();
-	return (0);
+    print_alphabet();
+    return (0);
 }
