1283993e684914deae51a33e8609687d5ce76f56
JordannM83 <11457@holbertonstudents.com>
2025-06-23 10:22:59 +0200

    fix: replace putchar with _putchar in more_numbers function

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -19,9 +19,9 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			if (j > 9)
-			putchar('1');
-			putchar(j % 10 + '0');
+			_putchar('1');
+			_putchar(j % 10 + '0');
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
