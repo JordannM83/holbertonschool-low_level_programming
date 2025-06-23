68f8cfc906d3656526beb0df9a8ed91acfb5c028
JordannM83 <11457@holbertonstudents.com>
2025-06-23 11:11:05 +0200

    fix: correct structure of print_square function to ensure proper newline handling

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,12 +12,16 @@ void print_square(int size)
 {
 int i, j;
 
+if (size > 0)
+{
 for (i = 0; i < size; i++)
 {
 	for (j = 0; j < size; j++)
 	{
 		_putchar('#');
 	}
-	_putchar('\n');
 }
+_putchar('\n');
+}
+_putchar ('\n');
 }
