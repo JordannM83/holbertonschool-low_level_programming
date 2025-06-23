be85b6d7a739a736339c1bb3b6ce51ea3cc02fb3
JordannM83 <11457@holbertonstudents.com>
2025-06-23 09:53:09 +0200

    feat: add print_numbers function and main for testing

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the multiplication
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i=0; i<10; i++)
+	{
+		_putchar(i + '0');	
+	}
+	_putchar('\n');
+}
