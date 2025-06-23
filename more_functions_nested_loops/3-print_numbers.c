623357df444670ca4aadb4623f8e52d964a11d39
JordannM83 <11457@holbertonstudents.com>
2025-06-23 09:54:24 +0200

    fix: correct formatting and comments in print_numbers function

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * mul - multiplies two integers
- * @a: first integer
- * @b: second integer
- *
- * Return: the result of the multiplication
+ * print_numbers - prints the numbers from 0 to 9
+ * followed by a new line
  */
+
 void print_numbers(void)
 {
 	int i;
-	for (i=0; i<10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(i + '0');	
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
