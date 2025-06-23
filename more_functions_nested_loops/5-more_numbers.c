5724b9ab2bb860ca1f405ef39528cd38f6822803
JordannM83 <11457@holbertonstudents.com>
2025-06-23 10:21:47 +0200

    feat: implement more_numbers and print_line functions with main for testing

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * more_numbers - prints 10 times the numbers from 0 to 14,
+ * followed by a new line
+ *
+ * Numbers are separated by a space.
+ * If the number is greater than 9, it is printed with its tens digit.
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			putchar('1');
+			putchar(j % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: the number of times the character '_' should be printed
+ *
+ * If n is 0 or less, the function does not print anything.
+ */
+
+void print_line(int n)
+{
+int i;
+
+if (i < n)
+{
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+}
+_putchar('\n');
+}
