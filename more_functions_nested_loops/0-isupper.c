a9a9cc4606e0e2f0e39f56268b6cd554c30a906f
JordannM83 <11457@holbertonstudents.com>
2025-06-23 09:40:26 +0200

    feat: implement _isupper function to check for uppercase characters and add main for testing

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
