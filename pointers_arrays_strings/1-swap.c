d3c62fdb5fe72b97cfdfe67f0c394e7f2d435345
JordannM83 <11457@holbertonstudents.com>
2025-06-24 09:55:28 +0200

    feat: implement swap_int function and update main file

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: pointer to the first integer
+ * @b: pointer to the second integer
+ * This function takes two pointers to integers and swaps the values
+ * they point to.
+ */
+
+void swap_int(int *a, int *b)
+{
+	if (a != NULL && b != NULL)
+	{
+		int temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
