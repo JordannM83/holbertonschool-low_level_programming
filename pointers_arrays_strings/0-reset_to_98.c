da1eef19e9d06a2192ab5dc37a96f6abd33f7c26
JordannM83 <11457@holbertonstudents.com>
2025-06-24 09:49:54 +0200

    feat: add initial implementation of pointer functions and main file

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * reset_to_98 - updates the value of the integer pointed to by n to 98
+ * @n: pointer to an integer
+ * This function takes a pointer to an integer and sets the value
+ * of the integer it points to 98.
+ */
+
+void reset_to_98(int *n)
+{
+	if (n != NULL)
+	{
+		*n = 98;
+	}
+}
