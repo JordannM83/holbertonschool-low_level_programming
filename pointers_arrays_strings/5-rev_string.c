f801e7b37b5a9cdc1bac43f0a61481f47e94bc7e
JordannM83 <11457@holbertonstudents.com>
2025-06-24 18:36:30 +0200

    feat: add rev_string function to reverse a string in place and update header file

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * rev_string - Reverses a string in place.
+ * @s: The string to be reversed.
+ *
+ * Description: This function takes a pointer to a string and reverses
+ * the characters in the string in place, modifying the original string.
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	int end = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (end = length - 1; end > start; end--, start++)
+	{
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+	}
+}
