6a38c42851007597250fb45f0e80e6152da68700
JordannM83 <11457@holbertonstudents.com>
2025-06-25 09:29:32 +0200

    fix: remove unnecessary blank lines and improve documentation in puts_half function

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-
-
+/**
+ * puts_half - prints the second half of a string
+ * @str: the string to print
+ */
 void puts_half(char *str)
 {
 	int len = 0;
 	int i;
+
 	while (str[len] != '\0')
 	{
 		len++;
