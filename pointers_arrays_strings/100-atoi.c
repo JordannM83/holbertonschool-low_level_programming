92334969fe991beac1694af457e0aac74c1f258c
JordannM83 <11457@holbertonstudents.com>
2025-06-25 14:24:57 +0200

    feat: add _atoi function to convert strings to integers and update header file

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ * Return: The integer value of the string, or 0 if no numbers are found.
+ */
+int _atoi(char *s)
+{
+	int i = 0, sign = 1, result = 0, started = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] && (s[i] >= '0' && s[i] <= '9'))
+		{
+			started = 1;
+			result = result * 10 + (s[i] - '0');
+		}
+
+		if (started == 1 && (s[i + 1] < '0' || s[i + 1] > '9'))
+		{
+			break;
+		}
+		i++;
+	}
+	if (!started)
+		return (0);
+	return (result * sign);
+}
