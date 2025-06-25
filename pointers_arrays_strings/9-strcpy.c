e312b99e8c007d761ef2b14a1f131b97d3eaaeb1
JordannM83 <11457@holbertonstudents.com>
2025-06-25 10:27:33 +0200

    feat: add _strcpy function to copy strings and update header file

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte
+ * to the buffer pointed to by dest.
+ * @dest: pointer to the destination buffer
+ * @src: pointer to the source string
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	char *d = dest;
+
+	while ((*d++ = *src++) != '\0')
+		;
+	return (dest);
+}
