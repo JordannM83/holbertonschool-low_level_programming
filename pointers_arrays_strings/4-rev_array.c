bd90e6f58613cb00d1333cbcf800c06d227fbee7
JordannM83 <11457@holbertonstudents.com>
2025-06-26 13:20:43 +0200

    feat: add print_array and reverse_array functions with main test file

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * reverse_array - Reverses the content of an array of integers.
+ * @a: The array of integers to be reversed.
+ * @n: The number of elements in the array.
+ */
+
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = 0;
+
+	for (end = n - 1; end > start; end--, start++)
+	{
+		char temp = a[start];
+
+		a[start] = a[end];
+		a[end] = temp;
+	}
+}
