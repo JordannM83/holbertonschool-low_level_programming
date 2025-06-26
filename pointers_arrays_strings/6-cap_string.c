bd08348b80cf54aec8d650d83add5169e0e2d99d
JordannM83 <11457@holbertonstudents.com>
2025-06-26 18:05:50 +0200

    fix: update cap_string function to use character literals for space and remove redundant checks

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,23 +11,13 @@
 char *cap_string(char *str)
 {
 int i = 0;
+
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
+		if (str[i] == (char)32 || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
 			str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
 			str[i] == (char)34 || str[i] == (char)40 || str[i] == (char)41 ||
 			str[i] == '{' || str[i] == '}')
-		{
-			if ((str[i + 1] >= 'a' && str[i + 1] <= 'z') && i != 0)
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
-		}
-		else if (i == 0 && (str[0] >= 'a' && str[0] <= 'z'))
-		{
-			str[0] = str[0] - 32;
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
 		{
 			if ((str[i + 1] >= 'a' && str[i + 1] <= 'z') && i != 0)
 			{
