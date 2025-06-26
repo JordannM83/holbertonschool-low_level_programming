55a43883e14134ea15229291f6b34369af43a351
JordannM83 <11457@holbertonstudents.com>
2025-06-26 18:01:23 +0200

    fix: update cap_string function to correctly capitalize words based on delimiters

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,8 +13,21 @@ char *cap_string(char *str)
 int i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] < (char)48 || str[i] > (char)122 ||
-		(str[i] > (char)57 && str[i] < (char)65))
+		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' ||
+			str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' ||
+			str[i] == (char)34 || str[i] == (char)40 || str[i] == (char)41 ||
+			str[i] == '{' || str[i] == '}')
+		{
+			if ((str[i + 1] >= 'a' && str[i + 1] <= 'z') && i != 0)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+		else if (i == 0 && (str[0] >= 'a' && str[0] <= 'z'))
+		{
+			str[0] = str[0] - 32;
+		}
+		else if (str[i] >= 'A' && str[i] <= 'Z')
 		{
 			if ((str[i + 1] >= 'a' && str[i + 1] <= 'z') && i != 0)
 			{
