4566bc5e7914c98539ab06818bf5a26fda8cf427
JordannM83 <11457@holbertonstudents.com>
2025-06-26 11:18:56 +0200

    feat: implement _strcat function for string concatenation and add main test file

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string to which src is appended.
+ * @src: The source string to be appended to dest.
+ * Return: A pointer to the resulting string dest.
+ */
 
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
 
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr++ = *src++;
+	}
+	*ptr = '\0';
 
+	return (dest);
+}
