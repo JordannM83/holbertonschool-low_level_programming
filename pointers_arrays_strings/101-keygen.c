0ae125240bca4a4416ce5c1afbe705f99623aeaf
JordannM83 <11457@holbertonstudents.com>
2025-06-28 12:03:54 +0200

    fix: simplify last character calculation and improve readability in 101-keygen

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -16,27 +16,23 @@ int main(void)
 {
 	char password[85];
 	int index = 0, sum = 0, target = 2772;
-	int needed;
 
 	srand(time(NULL));
 
-	/* Generate random characters for first 83 positions */
+	/* Generate random characters for 83 positions */
 	for (index = 0; index < 83; index++)
 	{
-		password[index] = (rand() % 94) + 33; /* printable ASCII characters */
+		password[index] = (rand() % 94) + 33;
 		sum += password[index];
 	}
 
-	/* Calculate what we need for the last character */
-	needed = target - sum;
+	/* Set the last character to reach target sum */
+	password[83] = (target - sum);
+	
+	/* Make sure it's printable */
+	if (password[83] < 33 || password[83] > 126)
+		password[83] = ((target - sum) % 94) + 33;
 
-	/* Adjust to printable range if needed */
-	if (needed < 33)
-		needed = 33 + (needed % 94);
-	else if (needed > 126)
-		needed = 33 + (needed % 94);
-
-	password[83] = needed;
 	password[84] = '\0';
 	printf("%s\n", password);
 	return (0);
