82da3c1744037d7c16d52bfd3c41744ec0aad653
JordannM83 <11457@holbertonstudents.com>
2025-06-28 11:52:14 +0200

    fix: correct password generation logic and improve output formatting in 101-keygen

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -3,25 +3,28 @@
 #include <time.h>
 #include <stdlib.h>
 
+#define ASCII_PRINTABLE_RANGE 94
+#define ASCII_PRINTABLE_START 33
+
 /**
  * main - Generates a random valid password for the program 101-crackme.
  *
  * Return: Always 0 (Success).
  */
+
 int main(void)
 {
-	char password[84];
+	char password[85];
 	int index = 0;
 
+	srand(time(NULL));
 	for (index = 0; index < 84; index++)
 	{
-		password[index] = rand();
+		password[index] = (rand() % ASCII_PRINTABLE_RANGE) + ASCII_PRINTABLE_START; /* printable ASCII characters */
+		password[index] = (rand() % 94) + 33; /* printable ASCII characters */
 	}
+	password[index] = '\0';
 	for (index = 0; index < 84; index++)
-	{
-		printf("%c", password[index]);
-	}
-		printf("%c", password[index - 1]);
-	printf("\n");
-	return (0);
+printf("%s\n", password);
+return (0);
 }
