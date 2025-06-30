91a4e08d40934ba8036d4f96b2f876a0ca0a2ee1
JordannM83 <11457@holbertonstudents.com>
2025-06-30 21:20:07 +0200

    feat: add print_chessboard function and main implementation to display chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: 2D array representing the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	while (a[i] != '\0')
+	{
+		while (a[i][j] != '\0')
+		{
+			
+		}
+	}
+}
