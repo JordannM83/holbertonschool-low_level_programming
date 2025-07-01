18d93bba5852dc0f8ccc934d8595232fa244aaf5
JordannM83 <11457@holbertonstudents.com>
2025-07-01 21:25:25 +0200

    fix: improve _atoi function logic and ensure proper sign handling

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,35 +10,27 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, sign = 1, started = 0;
+	int i = 0;
+	int sign = 1;
+	int number = 0;
 	double result = 0;
 
-
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		i++;
 		if (s[i] >= '0' && s[i] <= '9')
-		break;
-	}
-	i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] && (s[i] >= '0' && s[i] <= '9'))
 		{
-			started = 1;
-
+			number = 1;
 			result = result * 10 + (s[i] - '0');
 		}
-		if (started == 1 && (s[i + 1] < '0' || s[i + 1] > '9'))
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		if (number == 1 && (s[i + 1] < '0' || s[i + 1] > '9'))
 		{
 			break;
 		}
 		i++;
 	}
-	if (!started)
-		return (0);
 	return (result * sign);
 }
