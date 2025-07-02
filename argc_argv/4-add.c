8296278ab679dd5e0b1aac82e88faa188b8d04e2
JordannM83 <11457@holbertonstudents.com>
2025-07-02 16:35:08 +0200

    feat: refactor addition program to handle multiple positive numbers from command line arguments

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argv, char *argc[])
-{
-	if (argv == 3)
-	{
-	int num1 = atoi(argc[1]);
-	int num2 = atoi(argc[2]);
-	int result = num1 * num2;
+/**
+ * main - adds all positive numbers passed as command line arguments
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: Always 0.
+ */
 
-	printf("%d\n", result);
-	}
-	else
+int main(int argc, char *argv[])
+{
+	int num1;
+	int result = 0;
+	int i;
+
+	if (argc < 2)
 	{
-		printf("Error\n");
+		printf("0\n");
+		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		char *p = argv[i];
+		int j = 0;
+
+		while (p[j])
+		{
+			if (p[j] < '0' || p[j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		num1 = atoi(argv[i]);
+		result += num1;
+	}
+	printf("%d\n", result);
 	return (0);
 }
