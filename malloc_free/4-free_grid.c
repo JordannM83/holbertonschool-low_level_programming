a6ecbd0239c4f2bb80dc1df55fe8ca00423052dc
JordannM83 <11457@holbertonstudents.com>
2025-07-08 09:45:08 +0200

    feat: implement free_grid function to free a 2D grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -5,10 +5,16 @@
  * free_grid - frees a 2-dimensional grid previously created by alloc_grid
  * @grid: pointer to the grid to be freed
  * @height: height of the grid
- *
  * Return: Nothing.
  */
+
 void free_grid(int **grid, int height)
 {
-	
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
