0d17651f8a8396e9784e20b7e17e0366cb0e9e99
JordannM83 <11457@holbertonstudents.com>
2025-07-08 15:14:04 +0200

    fix: adjust memory allocation in _calloc to include space for an additional element

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	grid = malloc(nmemb * size);
+	grid = malloc(nmemb * size + 1);
 
 	if (grid == NULL)
 		return (NULL);
