e8858472b289f3590e4900808737240496db2483
JordannM83 <11457@holbertonstudents.com>
2025-07-08 14:03:25 +0200

    docs: update function documentation for string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
 
-
+/**
+ * string_nconcat - concat two strings up to n bytes of the second string
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes from s2 to concatenate
+ * Return: pointer to the newly allocated space in memory containing the
+ * concatenated string, or NULL if allocation fails
+ */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
