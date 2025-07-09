7a16ec20dffa6a0f81f1e726976fbb02534e40d5
JordannM83 <11457@holbertonstudents.com>
2025-07-09 09:47:07 +0200

    feat: add dog structure definition and main function for testing

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include "main.h"
+#include <stdlib.h>
+/**
+ * struct dog - structure to define a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
