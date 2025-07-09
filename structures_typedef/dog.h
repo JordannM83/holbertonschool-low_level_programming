6e1effad1bc711739933e747d5a0fab60b3b16d8
JordannM83 <11457@holbertonstudents.com>
2025-07-09 10:33:53 +0200

    feat: implement dog structure and initialization function

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-#include "main.h"
 #include <stdlib.h>
 /**
  * struct dog - structure to define a dog
@@ -14,5 +13,7 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
