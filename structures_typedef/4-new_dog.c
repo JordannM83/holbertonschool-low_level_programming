2488e542f3180dfccbf44da8ce564bfdc967413d
JordannM83 <11457@holbertonstudents.com>
2025-07-09 12:01:18 +0200

    feat: implement new_dog function and main program for dog structure testing

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Return: pointer to the new dog structure,
+ * or NULL if memory allocation fails
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	
+}
