87aa139bdb7b69da2bc229bf948a2cd5c09b99c7
JordannM83 <11457@holbertonstudents.com>
2025-07-14 18:27:56 +0200

    style: refactor main function for improved variable initialization and readability

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -5,15 +5,15 @@
 
 /**
  * main - performs simple operations
+ * @argc: the number of arguments
  * @argv: the array of arguments
  * Return: 0 on success, or exits with an error code.
  */
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	char *sign = argv[2];
+	int num1, num2;
+	char *sign;
 	int (*op_func)(int, int) = NULL;
 
 	if (argc != 4)
@@ -21,25 +21,24 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	else if (strcmp(sign, "+") != 0 && strcmp(sign, "-") != 0
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	sign = argv[2];
+
+	if (strcmp(sign, "+") != 0 && strcmp(sign, "-") != 0
 		&& strcmp(sign, "*") != 0 && strcmp(sign, "/") != 0
 		&& strcmp(sign, "%") != 0)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	else if ((strcmp(sign, "/") == 0 || strcmp(sign, "%") == 0) && num2 == 0)
+	if ((strcmp(sign, "/") == 0 || strcmp(sign, "%") == 0) && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 	op_func = get_op_func(sign);
-
-	if (op_func == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
 	printf("%d\n", op_func(num1, num2));
 	return (0);
 }
