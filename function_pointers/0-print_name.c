c202755f07cf068c99df00b9c95612063cca6cfd
JordannM83 <11457@holbertonstudents.com>
2025-07-14 18:10:22 +0200

    style: format code for consistency and readability

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - prints a name using the function pointer f
@@ -10,8 +10,8 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-
-	f(name);
+    if (name != NULL && f != NULL)
+    {
+        f(name);
+    }
 }
