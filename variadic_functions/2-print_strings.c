4425fda406ef264796fbbc2727495e757a65e87d
JordannM83 <11457@holbertonstudents.com>
2025-07-15 10:46:56 +0200

    style: add print_strings function implementation and update header file

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
+
+		if (separator != NULL && i > 0)
+		{
+			printf("%s", separator);
+		}
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", str);
+		}
+	}
+
+	va_end(args);
+	printf("\n");
+}
