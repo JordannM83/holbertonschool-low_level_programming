a4fc9397c067bdff9664f280e33af8dcf96c7579
JordannM83 <11457@holbertonstudents.com>
2025-07-15 09:43:38 +0200

    style: add variadic functions implementation with main function and header file

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of parameters
+ * * Return: sum of all parameters, or 0 if n is 0
+ */
+
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int result = 0;
+
+	va_start(args, n);
+
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; ++i)
+		result += va_arg(args, int);
+	va_end(args);
+	return (result);
+}
