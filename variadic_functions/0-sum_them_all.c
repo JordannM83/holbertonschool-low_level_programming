ffa8e66c5c3eac48a33b1299003b27e4405b7797
JordannM83 <11457@holbertonstudents.com>
2025-07-15 10:59:00 +0200

    style: fix formatting in sum_them_all function and ensure proper va_end call; add newline at end of print_strings file

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -17,8 +17,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (n == 0)
+	if (n == 0) {
+		va_end(args);
 		return (0);
+	}
 	for (i = 0; i < n; ++i)
 		result += va_arg(args, int);
 	va_end(args);
