97d76f2f9f9009b128b82fa2d3158a9246d26c12
JordannM83 <11457@holbertonstudents.com>
2025-07-16 10:24:28 +0200

    style: refactor print_all function to handle string arguments and improve null checks

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,10 +1,43 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 
 
 void print_all(const char * const format, ...)
 {
+unsigned int i = 0;
+char *str;
+va_list args;
 
+va_start(args, format);
+
+if (format == NULL || args == NULL)
+	return;
+
+while (format && format[i])
+{
+	str = va_arg(args, char *);
+	if (format[i])
+	{
+
+	}
+	else if ()
+	{
+		
+	}
+	else if (args == NULL && format[i] == "s")
+	{
+		printf("(nil)");
+	}
+	else if (format[i] == "s")
+	{
+		printf("%s", format[i])
+	}
+	i++;
+}
+
+va_end(args);
+printf('\n');
 }
