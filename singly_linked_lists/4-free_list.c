594d1fed92043850ef98e6c4b7765a84d9cd0d3a
JordannM83 <11457@holbertonstudents.com>
2025-07-28 14:28:52 +0200

    feat: implement add_node_end function to add a new node at the end of a list and free_list function to free a list_t list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,9 +1,23 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-
-
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list
+ */
 void free_list(list_t *head)
 {
-	
+	/* Déclarer un pointeur temporaire pour parcourir */
+	list_t *temp;
+	/* Déclarer un pointeur pour sauvegarder le nœud suivant */
+	while (head != NULL)
+	{
+		temp = head->next;
+
+		free(head->str);
+		free(head);
+		head = temp;
+
+	}
 }
