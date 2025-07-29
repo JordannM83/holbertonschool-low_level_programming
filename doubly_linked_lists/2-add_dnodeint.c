a0be361f07501bad39a56f0835767e679c4662ab
JordannM83 <11457@holbertonstudents.com>
2025-07-29 14:30:37 +0200

    refactor: remove redundant null check for new_node in add_dnodeint function

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -20,12 +20,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	new_node->next = *head;
+	new_node->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new_node;
 	*head = new_node;
 
 	return (new_node);
