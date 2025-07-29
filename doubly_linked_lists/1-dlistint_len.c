ff32d40c31429508cc703e44b1f5a4e7da80448b
JordannM83 <11457@holbertonstudents.com>
2025-07-29 09:50:37 +0200

    feat: implement add_dnodeint and add_dnodeint_end functions with error handling

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -10,5 +10,15 @@
 
 size_t dlistint_len(const dlistint_t *h)
 {
-	
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	if (h == NULL)
+		return (0);
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	return (count);
 }
--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -12,5 +12,21 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new_node;
 
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
