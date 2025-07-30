2bd35aacac684ac171ecc1cdc06611105192661c
JordannM83 <11457@holbertonstudents.com>
2025-07-30 11:23:41 +0200

    feat: add file I/O functions and main program for text file operations

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ *         0 if file can't be opened or read, filename is NULL,
+ *         or write fails
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/* Implementation goes here */
+	return (0);
+}
---
+++ file_io/3-cp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, exit codes 97-100 on various failures
+ */
+int main(int argc, char *argv[])
+{
+	/* Implementation goes here */
+	return (0);
+}
