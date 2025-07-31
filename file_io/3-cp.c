10d4f4b45ddff369edfc3489dea20c874a233f24
JordannM83 <11457@holbertonstudents.com>
2025-07-31 14:38:30 +0200

    feat: implement file copy functionality with error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,5 +1,79 @@
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
+/**
+ * close_file - closes a file and exits with code 100 if it fails
+ * @fd: file descriptor to close
+ */
+void close_file(int fd)
+{
+	int result;
+
+	result = close(fd);
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * open_files - opens source and destination files
+ * @argv: array of arguments
+ * @fd_from: pointer to source file descriptor
+ * @fd_to: pointer to destination file descriptor
+ */
+void open_files(char *argv[], int *fd_from, int *fd_to)
+{
+	*fd_from = open(argv[1], O_RDONLY);
+	if (*fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	*fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (*fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_file(*fd_from);
+		exit(99);
+	}
+}
+
+/**
+ * copy_file - copies content from source to destination file
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @argv: array of arguments for error messages
+ */
+void copy_file(int fd_from, int fd_to, char *argv[])
+{
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1 || bytes_written != bytes_read)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(99);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close_file(fd_from);
+		close_file(fd_to);
+		exit(98);
+	}
+}
+
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
@@ -9,6 +83,18 @@
  */
 int main(int argc, char *argv[])
 {
-	/* Implementation goes here */
+	int fd_from, fd_to;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	open_files(argv, &fd_from, &fd_to);
+	copy_file(fd_from, fd_to, argv);
+	close_file(fd_from);
+	close_file(fd_to);
+
 	return (0);
 }
