f35dca825a2d85b1a788e3c177ae509c7cebe8f7
JordannM83 <11457@holbertonstudents.com>
2025-07-31 14:47:35 +0200

    fix: streamline open_files and copy_file functions for improved error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,16 +33,12 @@ void open_files(char *argv[], int *fd_from, int *fd_to)
 		exit(98);
 	}
 
-	*fd_to = open(argv[2], O_WRONLY | O_TRUNC);
+	*fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (*fd_to == -1)
 	{
-		*fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-		if (*fd_to == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close_file(*fd_from);
-			exit(99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close_file(*fd_from);
+		exit(99);
 	}
 }
 
@@ -54,24 +50,18 @@ void open_files(char *argv[], int *fd_from, int *fd_to)
  */
 void copy_file(int fd_from, int fd_to, char *argv[])
 {
-	ssize_t bytes_read, bytes_written, total_written;
+	ssize_t bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
 
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		total_written = 0;
-		while (total_written < bytes_read)
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
 		{
-			bytes_written = write(fd_to, buffer + total_written,
-								bytes_read - total_written);
-			if (bytes_written == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				close_file(fd_from);
-				close_file(fd_to);
-				exit(99);
-			}
-			total_written += bytes_written;
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(99);
 		}
 	}
 
