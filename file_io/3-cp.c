2c9fac7f056c5292214e8cd6c64f64e844b1a82b
JordannM83 <11457@holbertonstudents.com>
2025-08-02 11:30:14 +0200

    fix: remove redundant close calls in copy_data for improved error handling

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -35,8 +35,6 @@ void copy_data(int fd_from, int fd_to, char **argv)
 		if (bytes_written == -1 || bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
 			exit(99);
 		}
 	}
@@ -44,8 +42,6 @@ void copy_data(int fd_from, int fd_to, char **argv)
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
 		exit(98);
 	}
 }
