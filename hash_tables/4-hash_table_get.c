651626c9c6a2cc5d4183d57d89b534841b6d3044
JordannM83 <11457@holbertonstudents.com>
2025-08-02 09:25:40 +0200

    feat: implement hash_table_get function to retrieve values by key

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,6 +9,16 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	/* TODO: Implement hash table get function */
-	return (NULL);
+	unsigned long int index;
+	hash_node_t *current;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+	index = hash_djb2((unsigned char *)key);
+	index %= ht->size;
+	current = ht->array[index];
+
+	if (current == NULL)
+		return (NULL);
+	return (current->value);
 }
